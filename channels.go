9abc96a13a07054f42d42b1faacf822967086002
Sandy <sandy@sandyuraz.com>
2023-08-30 20:56:11 -0500

    Added channel Collect
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- channels.go
+++ channels.go
@@ -1,6 +1,6 @@
 package rei
 
-// drain is a helper function that drains a channel. It is meant to be
+// Drain is a helper function that drains a channel. It is meant to be
 // used as a goroutine.
 func Drain[T any](vv chan T) {
 	for v := range vv {
@@ -8,5 +8,14 @@ func Drain[T any](vv chan T) {
 	}
 }
 
-// noop is a helper function that does nothing.
+// Noop is a helper function that does nothing.
 func Noop[T any](v T) {}
+
+// Collect returns all channel elements in a slice.
+func Collect[T any](vv chan T) []T {
+	ret := make([]T, 0, min(cap(vv), 1))
+	for v := range vv {
+		ret = append(ret, v)
+	}
+	return ret
+}
